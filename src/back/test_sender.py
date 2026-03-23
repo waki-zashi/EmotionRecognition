457fc288bb6e1fec6dc2dbf3eb0a7cb90c56356e
waki_zashi <xxeeggoorr@gmail.com>
2026-03-24 00:07:40 +0300

    fix sender

--- src/back/test_sender.py
+++ src/back/test_sender.py
@@ -1,13 +1,18 @@
 import redis
 import base64
 import json
+import time
+import uuid
+
 r = redis.Redis(host="localhost", port=6379, decode_responses=True)
 
 with open("test.jpg", "rb") as f:
     image_base64 = base64.b64encode(f.read()).decode()
 
+task_id = str(uuid.uuid4())
+
 task = {
-    "task_id": "test",
+    "task_id": task_id,
     "user_id": 1,
     "file_name": "test.jpg",
     "image_data": image_base64
@@ -18,5 +23,10 @@ r.lpush("image_tasks", json.dumps(task))
 print("Task sent")
 
 while True:
-    _, result = r.brpop("image_results")
-    print("Result:", result)
+    result = r.get(f"result: {task_id}")
+
+    if result:
+        print("Result:", result)
+        break
+
+    time.sleep(1)
