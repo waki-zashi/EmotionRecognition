8bbc1a40d734c9b3834ad03a7bfb7aef0f952652
maksim <maksimkasimov180@gmail.com>
2026-03-26 20:46:31 +0300

    docker-connected

--- src/backend/main.go
+++ src/backend/main.go
@@ -33,6 +33,7 @@ func authMiddleware(next http.HandlerFunc) http.HandlerFunc {
 
 func corsMiddleware(next http.HandlerFunc) http.HandlerFunc {
 	return func(w http.ResponseWriter, r *http.Request) {
+
 		w.Header().Set("Access-Control-Allow-Origin", "*")
 		w.Header().Set("Access-Control-Allow-Methods", "POST, GET, OPTIONS")
 		w.Header().Set("Access-Control-Allow-Headers", "Content-Type, X-Auth-Token")
